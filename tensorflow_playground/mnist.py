a42aa35cbc8a684dcc8937f3d35b86a644e463b5
Raphael Mader <RaphMad@users.noreply.github.com>
2019-01-25 15:46:31 +0100

    Renamed file to mnist.py

--- tensorflow_playground/mnist.py
+++ tensorflow_playground/mnist.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+
 mnist = tf.keras.datasets.mnist
 
 (x_train, y_train),(x_test, y_test) = mnist.load_data()
@@ -10,6 +11,7 @@ model = tf.keras.models.Sequential([
   tf.keras.layers.Dropout(0.2),
   tf.keras.layers.Dense(10, activation=tf.nn.softmax)
 ])
+
 model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
